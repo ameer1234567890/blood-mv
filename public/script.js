9c2117fde48fa069c671ed231404af2b96416585
Ameer Dawood <ameer1234567890@gmail.com>
2018-10-17 16:29:39 +0500

    a2hs really

--- public/script.js
+++ public/script.js
@@ -593,16 +593,10 @@ document.getElementById('menu-toggle').addEventListener('click', function() {
 });
 
 
-btnInstall.addEventListener('click', () => {
-  installPromptEvent.prompt();
-  installPromptEvent.userChoice.then((choice) => {
-    if (choice.outcome === 'accepted') {
-      console.log('User accepted the A2HS prompt');
-    } else {
-      console.log('User dismissed the A2HS prompt');
-    }
-    installPromptEvent = null;
-  });
+window.addEventListener('beforeinstallprompt', function (e) {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  e.prompt();
 });
 
 
