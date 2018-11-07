744e2548a69e93f8fa5187916f24c6a0e5daf8c6
Ameer Dawood <ameer1234567890@gmail.com>
2018-11-07 18:00:52 +0500

    Add GA with offline

--- public/js/init.js
+++ public/js/init.js
@@ -90,6 +90,7 @@ function updateContent(stateObj, pageURL) {
       $('#nav-mobile > li.active').removeClass('active');
       $('#' + desktopMenuElement).addClass('active');
       $('#' + mobileMenuElement).addClass('active');
+      ga('send', 'pageview');
     });
   }
 }
@@ -342,3 +343,12 @@ if ('serviceWorker' in navigator) {
     throw e;
   });
 }
+
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-128907524-1', 'auto');
+ga('set', 'dimension1', 'online');
+ga('send', 'pageview');
--- public/sw.js
+++ public/sw.js
@@ -95,3 +95,14 @@ messaging.setBackgroundMessageHandler(function(payload) {
 
   return self.registration.showNotification(notificationTitle, notificationOptions);
 });
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+workbox.googleAnalytics.initialize({
+  parameterOverrides: {
+    dimension1: 'offline',
+  },
+  hitFilter: (params) => {
+    const queueTimeInSeconds = Math.round(params.get('qt') / 1000);
+    params.set('metric1', queueTimeInSeconds);
+  },
+});
