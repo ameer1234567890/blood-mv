c5b420e8aa5fca6c99fa6d0aa2c8c1ad9ff1bf1c
Ameer Dawood <ameer1234567890@gmail.com>
2018-11-09 00:34:28 +0500

    Add admin interface v2

--- public/js/admin.js
+++ public/js/admin.js
@@ -15,12 +15,26 @@ $('#add-claim').on('click', function() {
           const json = JSON.parse(data);
           if (json && json.status == 'success') {
             firebase.auth().currentUser.getIdToken(true);
+            console.log('User token refreshed');
           }
         }
       });
   }).catch((error) => {
     console.log(error);
   });
+});
+
+
+firebase.auth().currentUser.getIdTokenResult().then((idTokenResult) => {
+  console.log(idTokenResult.claims.admin);
+   if (!!idTokenResult.claims.admin) {
+     console.log('You are admin');
+   } else {
+     console.log('You are not admin');
+   }
+})
+.catch((error) => {
+  console.log(error);
 });
 
 
