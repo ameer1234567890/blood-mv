982f9cfaac8fbc61d67ee2b4b76bc6fe7e07a71e
Ameer Dawood <ameer1234567890@gmail.com>
2018-11-22 09:16:23 +0500

    Cleanup TODO

--- public/js/init.js
+++ public/js/init.js
@@ -339,7 +339,6 @@ firebase.auth().onAuthStateChanged(function(user) {
 
 // Handle incoming messages. Called when:
 // - a message is received while the app has focus
-// - the user clicks on an app notification created by a service worker `messaging.setBackgroundMessageHandler` handler. // TODO: Check this condition
 messaging.onMessage(function(payload) {
   console.log('Message received. ', payload);
   M.toast({
